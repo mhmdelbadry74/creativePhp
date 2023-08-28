efc05b85a7d3362dd5f25c9ba27d181ef09b7d36
mhmdelbadry74 <m7mdelbadry72@gmail.com>
2023-08-28 07:54:08 +0300

    stop to layout cycle

--- public/index.php
+++ public/index.php
@@ -3,11 +3,18 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 use app\core\Application ;
-$app = new Application();
+$app = new Application(dirname(__DIR__));
 
+
+/* example use anonymous function
 $app->router->get('/' , function (){
     echo "hello world " ;
 });
+
+ end example  */
+
+$app->router->get('/' , 'home');
+$app->router->get('/welcome' , 'welcome');
 
 
 $app->run();
